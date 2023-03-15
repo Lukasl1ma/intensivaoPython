e96e3e6f668a2a35777f3d2bf9e994759ae7cae8
Lukas Lima <lukaslimap@gmail.com>
2023-03-15 13:25:17 -0300

    prática da aula 1 - automação de processos

---
+++ automacaoProcessoSistemas/automacaoPaginaWeb.py
@@ -0,0 +1,33 @@
+import pyautogui
+import time
+
+#pyautogui.PAUSE(1,0)
+
+
+#Acessar o navegador
+pyautogui.press("win")
+time.sleep(2)
+pyautogui.click(x=874, y=340)
+pyautogui.write("Chrome")
+pyautogui.press("enter")
+
+#Acessar o site
+time.sleep(2)
+pyautogui.write("https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema")
+pyautogui.press("enter")
+
+#Fazer Login no Site
+time.sleep(2)
+pyautogui.click(x=910, y=341)
+pyautogui.write("lucas.lp")
+time.sleep(2)
+pyautogui.click(x=1006, y=408)
+pyautogui.write("123456")
+time.sleep(2)
+pyautogui.click(x=965, y=493)
+
+
+
+
+
+
---
+++ position.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+#Capturar a posição do mouse, para utilizar na função click
+time.sleep(5)
+print(pyautogui.position())
